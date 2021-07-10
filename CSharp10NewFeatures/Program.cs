4065a56f732225b2ee30b19378e8319a44d35a21
Guilherme Natal de mello <Gui_Natal@hotmail.com>
2021-07-10 09:15:51 +0100

    global namespace

---
+++ CSharp10NewFeatures/Program.cs
@@ -0,0 +1,6 @@
+﻿// C# 10 introduces a new pattern that lets you define namespace imports across an entire project using the global keyword.
+// It’s recommended that you place your global imports in a separate file (one for each project), possibly named usings.cs or imports.cs.
+// Check "Imports.cs" file
+global using System;
+
+Console.WriteLine("Hello World!");
